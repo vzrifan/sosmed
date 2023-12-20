67b385dc1c2ed55fa9f2e041f2f0ffd4b69341b9
vzrifan <vzrifan@gmail.com>
2023-12-20 15:33:38 +0700

    add controller admin

--- app/controllers/LoginAdmin.php
+++ app/controllers/LoginAdmin.php
@@ -4,8 +4,6 @@ class LoginAdmin extends Controller
 {
     public function index()
     {
-        $this->view("templates/header");
         $this->view("loginAdmin/index");
-        $this->view("templates/footer");
     }
 }
